33a40be4a89437b8a4ed3e667bfb3e6140fdae31
kapishan <kapishan.official@gmail.com>
2026-06-20 17:46:25 +0530

    filter and sort method use

--- react-app/src/course.jsx
+++ react-app/src/course.jsx
@@ -5,34 +5,36 @@ function CourseList(){
         {
             id:1,
             name:"html course",
-            price: "$199",
+            price: 199,
             image: html,
             rating: 5
         },
          {
             id:2,
             name:"css course",
-            price: "$200",
+            price: 200,
             image: html,
             rating: 4
         },
          {
             id:3,
             name:"js course",
-            price: "$280",
+            price: 400,
             image: html,
             rating: 5
         },
         {
             id:4,
             name:"react",
-            price:"$500",
+            price:500,
             image:html,
             rating: 6
 
         }
     ]
-    const coursesList = courses.map((course) => <Card key={course.id} name={course.name} price={course.price} image={course.image} rating = {course.rating}/>)
+    courses.sort((x,y) => y.price - x.price)
+const bestCourses = courses.filter((course) => course.price > 200)
+    const coursesList = bestCourses.map((course) => <Card key={course.id} name={course.name} price={course.price} image={course.image} rating = {course.rating}/>)
     return(
         <>
         {coursesList}
