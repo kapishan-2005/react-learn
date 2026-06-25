77c5e35594ee48e160d9159914c0fff381f974b5
kapishan <kapishan.official@gmail.com>
2026-06-25 20:54:47 +0530

    useState used to buy now button purchashed state insert

--- react-app/src/card.jsx
+++ react-app/src/card.jsx
@@ -1,17 +1,21 @@
 import IMG from "./assets/html.webp"
 const card1 = "HTML"
+import { useState } from "react";
 
 function Card(props) {
-   
-    function buyCourse(){
-        console.log(props.name,"purchesed");
-    }
+    const [purchased, setPurchased] = useState(false);
+
+    function buyCourse(discount){
+        console.log(props.name ," purchased with ", discount , "%discount");
+        setPurchased(true);   
+    }   
     return(
         <div className="card">
             <img src={IMG}  alt=""/>
             <h2>{props.name}</h2>
             <p>{props.price}</p>
-            <button onClick={buyCourse}>Buy now</button>
+            <button onClick={(event)=> {buyCourse(20)}}>Buy now</button>
+            <p>{purchased ? 'Already purchased' : "Get it Now"}</p>
         </div>
     );
   
